aa6f1e719e05fe58646673bab86aa22e16f77391
Eleanor <eleanor128@gmail.com>
2024-11-08 12:43:29 +0800

    screen synchronous done

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -8,5 +8,11 @@ namespace SignalRChat.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task SendPlayerPosition(int x, int y)
+        {
+            // 向所有連線的客戶端發送玩家的位置
+            await Clients.All.SendAsync("ReceivePlayerPosition", x, y);
+        }
     }
 }
